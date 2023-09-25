3ec03d19ae666d7139008a44d1be68488ead4e69
Khin Lei Yati Htun <yati@192.168.1.7>
2023-09-25 21:54:19 +0630

    package for editor

--- src/screens/Area Evaluation/TestScreen.js
+++ src/screens/Area Evaluation/TestScreen.js
@@ -1,39 +1,47 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React,{useEffect}from 'react'
-import RNImageTools from "react-native-image-tools";
+import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import RNImageTools from 'react-native-image-tools';
+import PhotoEditor from '@baronha/react-native-photo-editor';
 
 export default function TestScreen() {
-    useEffect(async () => {
-        const hasPermission = await RNImageTools.checkImageLibraryPermission();
-        if (!hasPermission) {
-            await RNImageTools.requestImageLibraryPermission();
-        }
+  useEffect(() => {
+    console.log('hello');
+    // const hasPermission = await RNImageTools.checkImageLibraryPermission();
+    // console.log('hasPermission',hasPermission);
+    // if (!hasPermission) {
+    //     await RNImageTools.requestImageLibraryPermission();
+    // }
+  }, []);
 
+  const callfun = async () => {
+    try {
+      const remoteURL =
+        'https://images.unsplash.com/photo-1634915728822-5ad85582837a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80';
+      await PhotoEditor.open({
+        path: remoteURL,
+        stickers: [],
+      });
 
-    }, [])
+      //   const selected = await RNImageTools.selectImage({});
 
-    const callfun = async () => {
-        try {
-            const selected = await RNImageTools.selectImage({});
+      //   console.log('chosen image', selected);
 
-            console.log("chosen image", selected);
+      // const uri = selected.uri;
 
-            // const uri = selected.uri;
-
-            // this.setState({
-            //   originalImageUri: uri,
-            //   editedImageUri: null,
-            //   selectedImage: uri
-            // });
-        } catch (e) {
-            console.log("cancelled", e);
-        }
+      // this.setState({
+      //   originalImageUri: uri,
+      //   editedImageUri: null,
+      //   selectedImage: uri
+      // });
+    } catch (e) {
+      console.log('cancelled', e);
     }
-    return (
-        <View>
-            <TouchableOpacity onPress={() => callfun()}>
-                <Text>TestScreen</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  };
+  return (
+    <View>
+      <TouchableOpacity onPress={() => callfun()}>
+        <Text>TestScreen</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
