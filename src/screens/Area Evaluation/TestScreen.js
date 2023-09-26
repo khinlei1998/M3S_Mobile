7afa2778094eb80528f85add779ab12dddf13a21
khinlei1998 <khinleiyatihtun98@gmail.com>
2023-09-26 14:24:19 +0630

    testing for photo editor

--- src/screens/Area Evaluation/TestScreen.js
+++ src/screens/Area Evaluation/TestScreen.js
@@ -1,47 +1,50 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import React, {useEffect} from 'react';
-import RNImageTools from 'react-native-image-tools';
-import PhotoEditor from '@baronha/react-native-photo-editor';
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+// import RNImageTools from 'react-native-image-tools';
+// import PhotoEditor from '@baronha/react-native-photo-editor';
+import RNFS from 'react-native-fs';
 
 export default function TestScreen() {
-  useEffect(() => {
-    console.log('hello');
-    // const hasPermission = await RNImageTools.checkImageLibraryPermission();
-    // console.log('hasPermission',hasPermission);
-    // if (!hasPermission) {
-    //     await RNImageTools.requestImageLibraryPermission();
-    // }
-  }, []);
+    useEffect(() => {
+        console.log('hello');
+        // const hasPermission = await RNImageTools.checkImageLibraryPermission();
+        // console.log('hasPermission',hasPermission);
+        // if (!hasPermission) {
+        //     await RNImageTools.requestImageLibraryPermission();
+        // }
+    }, []);
 
-  const callfun = async () => {
-    try {
-      const remoteURL =
-        'https://images.unsplash.com/photo-1634915728822-5ad85582837a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80';
-      await PhotoEditor.open({
-        path: remoteURL,
-        stickers: [],
-      });
+    const callfun = async () => {
+        try {
+            //   const remoteURL =
+            //     'https://images.unsplash.com/photo-1634915728822-5ad85582837a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80';
+            //   await PhotoEditor.open({
+            //     path: remoteURL,
+            //     stickers: [],
+            //   });
+            // PhotoEditor.Edit({
+            //     path: RNFS.DocumentDirectoryPath + "/photo.jpg"
+            // });
+            // const selected = await RNImageTools.selectImage();
 
-      //   const selected = await RNImageTools.selectImage({});
+            // console.log('chosen image', selected);
 
-      //   console.log('chosen image', selected);
+            // const uri = selected.uri;
 
-      // const uri = selected.uri;
-
-      // this.setState({
-      //   originalImageUri: uri,
-      //   editedImageUri: null,
-      //   selectedImage: uri
-      // });
-    } catch (e) {
-      console.log('cancelled', e);
-    }
-  };
-  return (
-    <View>
-      <TouchableOpacity onPress={() => callfun()}>
-        <Text>TestScreen</Text>
-      </TouchableOpacity>
-    </View>
-  );
+            // this.setState({
+            //   originalImageUri: uri,
+            //   editedImageUri: null,
+            //   selectedImage: uri
+            // });
+        } catch (e) {
+            console.log('cancelled', e);
+        }
+    };
+    return (
+        <View>
+            <TouchableOpacity onPress={() => callfun()}>
+                <Text>TestScreen</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
